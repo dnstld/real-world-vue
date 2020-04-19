dfb9247dcb31ffedf1f77e008166e86aad07df0f
Denis Toledo <contato@denistoledo.com.br>
2020-04-19 09:15:59 -0300

    use state and mapState

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
-    <h1>Event Create</h1>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.name }}</p>
+    <ul>
+      <li v-for="categorie in categories" :key="categorie">
+        {{ categorie }}
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  computed: mapState(["user", "categories"])
+};
+</script>
+
+<style scoped></style>
