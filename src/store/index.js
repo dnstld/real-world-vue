c7cbbd968e279af63ba2ebcccb9d10535edf0bb8
Denis Toledo <contato@denistoledo.com.br>
2020-04-19 15:46:46 -0300

    fetch API with actions and mutations

--- src/store/index.js
+++ src/store/index.js
@@ -23,7 +23,10 @@ export default new Vuex.Store({
   },
   mutations: {
     ADD_EVENT(state, event) {
-      return state.events.push(event);
+      state.events.push(event);
+    },
+    SET_EVENTS(state, events) {
+      state.events = events;
     }
   },
   actions: {
@@ -31,6 +34,15 @@ export default new Vuex.Store({
       return EventService.postEvent(event).then(() => {
         commit("ADD_EVENT", event);
       });
+    },
+    fetchEvents({ commit }) {
+      EventService.getEvents()
+        .then(response => {
+          commit("SET_EVENTS", response.data);
+        })
+        .catch(error => {
+          console.log("There was an error: " + error.response);
+        });
     }
   },
   modules: {},
