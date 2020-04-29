aa5db5e286c358cf037cf10f81a2e6e23d0da5a6
Denis Toledo <contato@denistoledo.com.br>
2020-04-29 16:57:46 -0300

    create base select component

--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -1,14 +1,27 @@
 <template>
   <div>
     <label v-if="label">{{ label }}</label>
-    <input :value="value" @input="updateValue" v-bind="$attrs" />
+    <select :value="value" @input="updateValue" v-bind="$attrs">
+      <option
+        v-for="option in options"
+        :key="option"
+        :selected="option === value"
+        >{{ option }}</option
+      >
+    </select>
   </div>
 </template>
+
+// Vue-multiselect library
 
 <script>
 export default {
   inheritAttrs: false,
   props: {
+    options: {
+      type: Array,
+      required: true
+    },
     label: {
       type: String,
       default: ""
