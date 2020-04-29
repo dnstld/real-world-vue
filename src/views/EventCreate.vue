5326d6fd477277e9b166a52dcbba93a9e451ca28
Denis Toledo <contato@denistoledo.com.br>
2020-04-29 16:39:50 -0300

    create base form component - BaseInput

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -6,22 +6,23 @@
         <option v-for="cat in categories" :key="cat">{{ cat }}</option>
       </select>
       <h3>Name & describe your event</h3>
-      <div class="field">
-        <label>Title</label>
-        <input
-          v-model="event.title"
-          type="text"
-          placeholder="Add an event title"
-        />
-      </div>
-      <div class="field">
-        <label>Description</label>
-        <input
-          v-model="event.description"
-          type="text"
-          placeholder="Add a description"
-        />
-      </div>
+
+      <BaseInput
+        label="Title"
+        v-model="event.title"
+        type="text"
+        placeholder="Add an event title"
+        class="field"
+      />
+
+      <BaseInput
+        label="Description"
+        v-model="event.description"
+        type="text"
+        placeholder="Add a description"
+        class="field"
+      />
+
       <h3>Where is your event?</h3>
       <div class="field">
         <label>Location</label>
